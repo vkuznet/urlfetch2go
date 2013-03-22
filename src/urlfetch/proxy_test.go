8767b28f7025242ade66feee0c7438e69db19efe
Valentin Kuznetsov <vkuznet@gmail.com>
2013-03-22 17:14:56 -0400

    Use regexp to match end of html; let local test function to return bool status

--- src/urlfetch/proxy_test.go
+++ src/urlfetch/proxy_test.go
@@ -1,30 +1,34 @@
 package urlfetch
 
 import "testing"
-import "fmt"
+import "regexp"
 
 // Local helper functions
-func test_getdata4urls(urls []string) {
-    // create HTTP client
-    client := HttpClient()
-
-    ch := make(chan []byte)
-    n := 0
-    for _, url := range urls {
-        n++
-        go Getdata(client, url, ch)
+func parse(output string) bool {
+    test, err := regexp.MatchString("</html>", output)
+    if  err != nil {
+        return false
     }
-    for i:=0; i<n; i++ {
-        fmt.Println(string(<-ch))
-    }
+    return test
 }
-func test_getdata(url string) {
-    // create HTTP client
-    client := HttpClient()
-
+func test_getdata4urls(urls []string) bool {
     ch := make(chan []byte)
-    go Getdata(client, url, ch)
-    fmt.Println(string(<-ch))
+    for _, url := range urls {
+        go Getdata(url, ch)
+    }
+    for i := 0; i<len(urls); i++ {
+        res := string(<-ch)
+        if  ! parse(res) {
+            return false
+        }
+    }
+    return true
+}
+func test_getdata(url string) bool {
+    ch := make(chan []byte)
+    go Getdata(url, ch)
+    res := string(<-ch)
+    return parse(res)
 }
 
 // Test function
@@ -32,8 +36,15 @@ func TestGetdata(t *testing.T) {
     url1 := "http://www.google.com"
     url2 := "http://www.golang.org"
     urls := []string{url1, url2}
-    t.Log("test getdata call")
-    test_getdata(url1)
-    t.Log("test getdata call with multiple urls")
-    test_getdata4urls(urls)
+    var test bool
+    test = test_getdata(url1)
+    if  ! test {
+        t.Log("test getdata call", url1)
+        t.Fail()
+    }
+    test = test_getdata4urls(urls)
+    if  ! test {
+        t.Log("test getdata call with multiple urls", urls)
+        t.Fail()
+    }
 }
